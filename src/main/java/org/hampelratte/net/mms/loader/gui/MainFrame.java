2059d019c1ba9c463e9f4a28efc3c63f1a82fd4f
Henrik Niehaus <henrik.niehaus@gmx.de>
2011-09-19 17:30:57 +0200

    make use of the new Client class. this way we can use the automatic protocol rollover and support mms and mms over http.

--- src/main/java/org/hampelratte/net/mms/loader/gui/MainFrame.java
+++ src/main/java/org/hampelratte/net/mms/loader/gui/MainFrame.java
@@ -9,10 +9,10 @@ import java.awt.event.ActionListener;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.net.ConnectException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.text.DecimalFormat;
-import java.util.UUID;
 
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
@@ -30,8 +30,7 @@ import org.hampelratte.net.mms.asf.io.ASFInputStream;
 import org.hampelratte.net.mms.asf.objects.ASFFilePropertiesObject;
 import org.hampelratte.net.mms.asf.objects.ASFObject;
 import org.hampelratte.net.mms.asf.objects.ASFToplevelHeader;
-import org.hampelratte.net.mms.client.MMSClient;
-import org.hampelratte.net.mms.client.MMSNegotiator;
+import org.hampelratte.net.mms.client.Client;
 import org.hampelratte.net.mms.client.listeners.MMSMessageListener;
 import org.hampelratte.net.mms.client.listeners.MMSPacketListener;
 import org.hampelratte.net.mms.data.MMSHeaderPacket;
@@ -41,11 +40,6 @@ import org.hampelratte.net.mms.io.MMSFileDumper;
 import org.hampelratte.net.mms.io.RemoteException;
 import org.hampelratte.net.mms.io.util.HRESULT;
 import org.hampelratte.net.mms.messages.MMSMessage;
-import org.hampelratte.net.mms.messages.client.Connect;
-import org.hampelratte.net.mms.messages.client.ConnectFunnel;
-import org.hampelratte.net.mms.messages.client.OpenFile;
-import org.hampelratte.net.mms.messages.client.ReadBlock;
-import org.hampelratte.net.mms.messages.client.StreamSwitch;
 import org.hampelratte.net.mms.messages.server.ReportEndOfStream;
 import org.hampelratte.net.mms.messages.server.ReportStreamSwitch;
 import org.slf4j.Logger;
@@ -54,18 +48,18 @@ import org.slf4j.LoggerFactory;
 public class MainFrame extends JFrame implements ActionListener, MMSMessageListener, MMSPacketListener {
 
     private static transient Logger logger = LoggerFactory.getLogger(MainFrame.class);
-    
+
     private JTextField tfUrl, tfDir;
     private JLabel lThroughput;
     private DecimalFormat df = new DecimalFormat("0.00");
     private JButton bBrowse, bStart;
     private JProgressBar progress;
-    
+
     private boolean running = false;
     private long packetCount, packetReadCount;
-    
-    private MMSClient client;
-    
+
+    private Client client;
+
     public MainFrame() {
         initGUI();
     }
@@ -74,14 +68,14 @@ public class MainFrame extends JFrame implements ActionListener, MMSMessageListe
         setSize(600, 200);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setTitle("MMS4J Loader");
-        
+
         setLayout(new BorderLayout());
         JPanel panel = new JPanel(new GridBagLayout());
         add(panel, BorderLayout.CENTER);
-        
+
         float WEIGHT_LIGHT = 0.2f;
         float WEIGHT_HEAVY = 0.8f;
-        
+
         // url label and textfield
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.gridx = gbc.gridy = 0;
@@ -89,41 +83,41 @@ public class MainFrame extends JFrame implements ActionListener, MMSMessageListe
         gbc.insets = new Insets(5, 5, 5, 5);
         gbc.weightx = WEIGHT_LIGHT;
         panel.add(new JLabel("URL"), gbc);
-        
+
         gbc.gridx = 1;
         gbc.weightx = WEIGHT_HEAVY;
         gbc.gridwidth = 2;
         gbc.fill = GridBagConstraints.HORIZONTAL;
         tfUrl = new JTextField();
         panel.add(tfUrl, gbc);
-        
+
         // directory label, textfield and button
         gbc.gridx = 0;
         gbc.gridy = 1;
         gbc.weightx = WEIGHT_LIGHT / 2;
         gbc.fill = GridBagConstraints.NONE;
         panel.add(new JLabel("Destination directory"), gbc);
-        
+
         gbc.gridx = 1;
         gbc.weightx = WEIGHT_HEAVY;
         gbc.gridwidth = 1;
         gbc.fill = GridBagConstraints.HORIZONTAL;
         tfDir = new JTextField();
         panel.add(tfDir, gbc);
-        
+
         gbc.gridx = 2;
         gbc.weightx = WEIGHT_LIGHT / 2;
         gbc.fill = GridBagConstraints.HORIZONTAL;
         bBrowse = new JButton("Browse...");
         bBrowse.addActionListener(this);
         panel.add(bBrowse, gbc);
-        
+
         // progress bar and label
         gbc.gridx = 0;
         gbc.gridy = 2;
         gbc.weightx = WEIGHT_LIGHT;
         panel.add(new JLabel("Progress"), gbc);
-        
+
         gbc.gridx = 1;
         gbc.weightx = WEIGHT_HEAVY;
         gbc.gridwidth = 2;
@@ -133,20 +127,20 @@ public class MainFrame extends JFrame implements ActionListener, MMSMessageListe
         progress.setStringPainted(true);
         progress.setValue(0);
         panel.add(progress, gbc);
-        
+
         // throughput
         gbc.gridx = 0;
         gbc.gridy = 3;
         gbc.weightx = WEIGHT_LIGHT;
         panel.add(new JLabel("Throughput"), gbc);
-        
+
         gbc.gridx = 1;
         gbc.weightx = WEIGHT_HEAVY;
         gbc.gridwidth = 2;
         gbc.fill = GridBagConstraints.HORIZONTAL;
         lThroughput = new JLabel();
         panel.add(lThroughput, gbc);
-        
+
         // start button
         gbc.gridx = 1;
         gbc.gridy = 4;
@@ -155,30 +149,29 @@ public class MainFrame extends JFrame implements ActionListener, MMSMessageListe
         bStart = new JButton("Download");
         bStart.addActionListener(this);
         panel.add(bStart, gbc);
-        
-        
-        
-        tfUrl.setText("mms://tagesschau.wmod.llnwd.net/a3705/d1/2010/0822/TV-20100822-1211-5001.wm.hi.wmv");
+
+        tfUrl.setText("mms://apasf.apa.at/cms-worldwide/2011-09-18_1100_sd_02_ZIB_____2913225__o__0000823759__s2917803___hr_ORF2HiRes_10595712P_11051213P.wmv");
+        // tfUrl.setText("mms://a1014.v1252931.c125293.g.vm.akamaistream.net/7/1014/125293/v0001/wm.od.origin.zdf.de.gl-systemhaus.de/none/zdf/11/05/110530_karriere_kuss_37g_vh.wmv");
         tfDir.setText("/tmp");
-        
+
         setVisible(true);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource() == bBrowse) {
+        if (e.getSource() == bBrowse) {
             JFileChooser jfc = new JFileChooser();
             jfc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
             int selection = jfc.showOpenDialog(this);
-            if(selection == JFileChooser.APPROVE_OPTION) {
+            if (selection == JFileChooser.APPROVE_OPTION) {
                 System.out.println("Save file in " + jfc.getSelectedFile());
                 tfDir.setText(jfc.getSelectedFile().getAbsolutePath());
             }
-        } else if(e.getSource() == bStart) {
+        } else if (e.getSource() == bStart) {
             running = !running;
-            if(running) {
+            if (running) {
                 setUserInteractionEnabled(false);
-                
+
                 String uri = tfUrl.getText();
                 String dir = tfDir.getText();
                 logger.info("Trying to download {} to directory {}", uri, dir);
@@ -218,53 +211,22 @@ public class MainFrame extends JFrame implements ActionListener, MMSMessageListe
             }
         }
     }
-    
+
     private void startDownload(String mmsUri, final String destDir) throws Exception {
         reset();
         bStart.setText("Cancel");
         progress.setIndeterminate(true);
         progress.setString("Connecting to server");
-        
+
         URI uri = new URI(mmsUri);
-        String host = uri.getHost();
+        client = new Client(uri);
+
         File path = new File(uri.getPath());
         String filepath = path.getParentFile().getPath();
-        if(filepath.startsWith("/")) {
+        if (filepath.startsWith("/")) {
             filepath = filepath.substring(1);
         }
         final String filename = path.getName();
-        
-        MMSNegotiator negotiator = new MMSNegotiator();
-        client = new MMSClient(host, 1755, negotiator);
-        negotiator.setClient(client);
-
-        // configure the negotiator
-        // connect
-        Connect connect = new Connect();
-        connect.setPlayerInfo("NSPlayer/7.0.0.1956");
-        connect.setGuid(UUID.randomUUID().toString());
-        connect.setHost(host);
-        negotiator.setConnect(connect);
-        // connect funnel
-        ConnectFunnel cf = new ConnectFunnel();
-        cf.setIpAddress("192.168.0.1");
-        cf.setProtocol("TCP");
-        cf.setPort("1037");
-        negotiator.setConnectFunnel(cf);
-        // open file
-        OpenFile of = new OpenFile();
-        String remoteFile = filepath + '/' +filename;
-        logger.info("Remote file is {}", remoteFile);
-        of.setFileName(remoteFile);
-        negotiator.setOpenFile(of);
-        // read block
-        ReadBlock rb = new ReadBlock();
-        negotiator.setReadBlock(rb);
-        // stream switch
-        StreamSwitch ss = new StreamSwitch();
-        ss.addStreamSwitchEntry(ss.new StreamSwitchEntry(0xFFFF, 1, 0));
-        ss.addStreamSwitchEntry(ss.new StreamSwitchEntry(0xFFFF, 2, 0));
-        negotiator.setStreamSwitch(ss);
 
         // dump the received header and media packets to a file
         File dump = new File(destDir, filename);
@@ -273,21 +235,32 @@ public class MainFrame extends JFrame implements ActionListener, MMSMessageListe
         client.addMessageListener(this);
         client.addPacketListener(dumper);
         client.addPacketListener(this);
+
         client.addAdditionalIoHandler(new IoHandlerAdapter() {
+            private int connectExceptionCount = 0;
+
             @Override
             public void exceptionCaught(IoSession session, Throwable cause) throws Exception {
+                // ignore the first connect exception, because the client will try another connection
+                // with the mms over http protocol
+                if (cause instanceof ConnectException) {
+                    if (++connectExceptionCount < 2) {
+                        return;
+                    }
+                }
+
                 logger.error("An error occured", cause);
                 reset();
                 progress.setString(cause.getLocalizedMessage());
                 setUserInteractionEnabled(true);
                 running = false;
-                
-                if(cause.getCause() != null && cause.getCause() instanceof RemoteException) {
+
+                if (cause.getCause() != null && cause.getCause() instanceof RemoteException) {
                     RemoteException re = (RemoteException) cause.getCause();
                     String msg = HRESULT.hrToHumanReadable(re.getHr());
                     progress.setString(msg);
                 }
-                
+
                 client.disconnect(new IoFutureListener<IoFuture>() {
                     @Override
                     public void operationComplete(IoFuture arg0) {
@@ -297,10 +270,9 @@ public class MainFrame extends JFrame implements ActionListener, MMSMessageListe
             }
         });
 
-        // finally start the download, the negotiator
-        // will start automatically when the connection is
-        // established
-        client.connect();
+        // finally connect to the server and start the download
+        // if the connection has been established
+        client.connect(null);
     }
 
     private void reset() {
@@ -312,7 +284,7 @@ public class MainFrame extends JFrame implements ActionListener, MMSMessageListe
         lThroughput.setText("");
         bStart.setText("Download");
     }
-    
+
     private void setUserInteractionEnabled(boolean enabled) {
         tfUrl.setEnabled(enabled);
         tfDir.setEnabled(enabled);
@@ -321,17 +293,17 @@ public class MainFrame extends JFrame implements ActionListener, MMSMessageListe
 
     @Override
     public void packetReceived(MMSPacket mmspacket) {
-        if(mmspacket instanceof MMSHeaderPacket) {
+        if (mmspacket instanceof MMSHeaderPacket) {
             MMSHeaderPacket hp = (MMSHeaderPacket) mmspacket;
             try {
                 ByteArrayInputStream bin = new ByteArrayInputStream(hp.getData());
                 ASFInputStream asfin = new ASFInputStream(bin);
-                ASFObject asfo = (ASFObject) asfin.readASFObject();
-                if(asfo instanceof ASFToplevelHeader) {
+                ASFObject asfo = asfin.readASFObject();
+                if (asfo instanceof ASFToplevelHeader) {
                     ASFToplevelHeader asfHeader = (ASFToplevelHeader) asfo;
                     logger.debug("ASF header: {}", asfHeader);
                     ASFFilePropertiesObject fileprops = (ASFFilePropertiesObject) asfHeader.getNestedHeader(ASFFilePropertiesObject.class);
-                    if(fileprops != null) {
+                    if (fileprops != null) {
                         packetCount = fileprops.getDataPacketCount();
                         progress.setIndeterminate(false);
                         logger.info("Stream consists of {} packets", packetCount);
@@ -343,12 +315,12 @@ public class MainFrame extends JFrame implements ActionListener, MMSMessageListe
             } catch (Exception e) {
                 logger.warn("Ignoring unknown ASF header object", e);
             }
-        } else if(mmspacket instanceof MMSMediaPacket) {
+        } else if (mmspacket instanceof MMSMediaPacket) {
             packetReadCount++;
             lThroughput.setText(df.format(client.getSpeed()) + " KiB/s");
-            int p = (int) ((double)packetReadCount / (double)packetCount * 100);
+            int p = (int) ((double) packetReadCount / (double) packetCount * 100);
             setProgess(p);
-            logger.info("{} / {} packets read", packetReadCount, packetCount);
+            logger.info("{} / {} packets read. {}", new Object[] { packetReadCount, packetCount, lThroughput.getText() });
         }
     }
 
@@ -356,24 +328,26 @@ public class MainFrame extends JFrame implements ActionListener, MMSMessageListe
         progress.setValue(percentage);
         progress.setString(percentage + " %");
     }
-    
+
     @Override
     public void messageReceived(MMSMessage command) {
         if (command instanceof ReportStreamSwitch) {
+            ReportStreamSwitch rss = (ReportStreamSwitch) command;
+            logger.debug("StreamSwitch result: {}", rss.getHr());
+
             // great, we can start the streaming
             long startPacket = 0;
 
             // start the streaming
-            progress.setString("Starting download");
             client.startStreaming(startPacket);
-        } else if(command instanceof ReportEndOfStream) {
+        } else if (command instanceof ReportEndOfStream) {
             reset();
             setProgess(100);
             progress.setString("Finished");
             lThroughput.setText("");
             setUserInteractionEnabled(true);
             running = false;
-            
+
             logger.info("Received end of stream. Closing connection.");
             client.disconnect(new IoFutureListener<IoFuture>() {
                 @Override
